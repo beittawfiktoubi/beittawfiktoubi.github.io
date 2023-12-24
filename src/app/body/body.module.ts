4022836b8b1a85b3b9c73ab524be08e8b2341c10
Arik Hativa <arikhativa@gmail.com>
2023-12-24 13:21:40 +0200

    basic header

--- src/app/body/body.module.ts
+++ src/app/body/body.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './header/header.component';
-import { IntroComponent } from './intro/intro.component';
-import { GalleryComponent } from './gallery/gallery.component';
-import { ActivitiesComponent } from './activities/activities.component';
-import { ReadMoreComponent } from './read-more/read-more.component';
-import { FooterComponent } from './footer/footer.component';
-
-
+import { HeaderComponent } from '../header/header.component';
+import { IntroComponent } from '../intro/intro.component';
+import { GalleryComponent } from '../gallery/gallery.component';
+import { ActivitiesComponent } from '../activities/activities.component';
+import { ReadMoreComponent } from '../read-more/read-more.component';
+import { FooterComponent } from '../footer/footer.component';
 
 @NgModule({
     declarations: [],
