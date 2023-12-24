c1e52cba8796fedbac16fb53c7aad6399c726cce
Arik Hativa <arikhativa@gmail.com>
2023-12-24 12:34:39 +0200

    mid init

---
+++ src/app/body/body.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { IntroComponent } from './intro/intro.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { ReadMoreComponent } from './read-more/read-more.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        HeaderComponent, IntroComponent, GalleryComponent, ActivitiesComponent, ReadMoreComponent, FooterComponent
+    ],
+    exports: [HeaderComponent, IntroComponent, GalleryComponent, ActivitiesComponent, ReadMoreComponent, FooterComponent]
+})
+export class BodyModule { }
---
+++ src/app/body/read-more/read-more.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-read-more',
+  standalone: true,
+  imports: [],
+  templateUrl: './read-more.component.html',
+  styleUrl: './read-more.component.scss'
+})
+export class ReadMoreComponent {
+
+}
