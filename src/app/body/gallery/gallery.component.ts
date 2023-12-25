58cb924e621ed90ba93619e9d49c81ff59d58450
Arik Hativa <arikhativa@gmail.com>
2023-12-25 14:51:03 +0200

    BasicGallery

--- src/app/body/gallery/gallery.component.ts
+++ src/app/body/gallery/gallery.component.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
 
+class imageList {
+    constructor(
+        public src: string,
+        public alt: string
+    ) { }
+}
+
 @Component({
-  selector: 'app-gallery',
-  standalone: true,
-  imports: [],
-  templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.scss'
+    selector: 'app-gallery',
+    standalone: true,
+    imports: [],
+    templateUrl: './gallery.component.html',
+    styleUrl: './gallery.component.scss'
 })
 export class GalleryComponent {
+    private readonly images: imageList[] = [
+        {
+            src: "/assets/images/gallery/1.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/2.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/3.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/4.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/5.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/6.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/7.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/8.jpg",
+            alt: "description",
+        },
+    ];
 
 }
