2a90729d92db3d0445e459d1dfb1e429d921efb1
Arik Hativa <arikhativa@gmail.com>
2024-01-04 11:30:42 +0200

    move is working

--- src/app/body/gallery/gallery.component.ts
+++ src/app/body/gallery/gallery.component.ts
@@ -29,29 +29,37 @@ export class GalleryComponent {
             src: "/assets/images/gallery/3.jpg",
             alt: "description",
         },
-        // {
-        //     src: "/assets/images/gallery/4.jpg",
-        //     alt: "description",
-        // },
-        // {
-        //     src: "/assets/images/gallery/5.jpg",
-        //     alt: "description",
-        // },
-        // {
-        //     src: "/assets/images/gallery/6.jpg",
-        //     alt: "description",
-        // },
-        // {
-        //     src: "/assets/images/gallery/7.jpg",
-        //     alt: "description",
-        // },
-        // {
-        //     src: "/assets/images/gallery/8.jpg",
-        //     alt: "description",
-        // },
+        {
+            src: "/assets/images/gallery/4.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/5.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/6.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/7.jpg",
+            alt: "description",
+        },
+        {
+            src: "/assets/images/gallery/8.jpg",
+            alt: "description",
+        },
     ];
 
-    private _currentImage: number = 2;
+    private readonly MOVE_LEFT: string = "translateX(-80vw)";
+    private readonly MOVE_RIGHT: string = "translateX(80vw)";
+    private readonly MOVE_CENTER: string = "translateX(0)";
+    private readonly SMALL_SIZE: string = "0.8";
+    private readonly REGULAR_SIZE: string = "1";
+
+    private _currentImage: number = 0;
+    private _leftZIndex: number = 0;
+    private _rightZIndex: number = this.images.length;
     private _images: HTMLCollectionOf<HTMLElement> = document.getElementsByClassName("img-container") as HTMLCollectionOf<HTMLElement>;
 
     ngAfterViewInit() {
@@ -59,17 +67,56 @@ export class GalleryComponent {
         this._resetArrayValues(this._images);
     }
 
+    private _imageMoveLeft(img: CSSStyleDeclaration) {
+        img.transform = this.MOVE_LEFT;
+        img.scale = this.SMALL_SIZE;
+        img.zIndex = this._leftZIndex.toString();
+    }
 
-    sendRight() {
-        this._images[this._currentImage].style.transform = "translateX(200%)";
-        this._currentImage--;
-        this._images[this._currentImage].style.transform = "translateX(0)";
+    private _imageMoveCenter(img: CSSStyleDeclaration) {
+        img.transform = this.MOVE_CENTER;
+        img.scale = this.REGULAR_SIZE;
+        img.zIndex = Math.max(this._rightZIndex, this._leftZIndex).toString();
+
+    }
+
+    private _imageMoveRight(img: CSSStyleDeclaration) {
+        img.transform = this.MOVE_RIGHT;
+        img.scale = this.SMALL_SIZE;
+        img.zIndex = this._rightZIndex.toString();
+    }
+
+    // <---- img
+    turnLeft() {
+        if (this._currentImage >= 0 && this._currentImage < this._images.length - 1) {
+            this._imageMoveLeft(this._images[this._currentImage].style);
+            this._currentImage++;
+            this._imageMoveCenter(this._images[this._currentImage].style);
+            console.log("LEFT", this._leftZIndex, this._rightZIndex);
+            this._leftZIndex++;
+            this._rightZIndex--;
+        }
+    }
+
+    // img ---->
+    turnRight() {
+        if (this._currentImage > 0 && this._currentImage <= this._images.length - 1) {
+            this._imageMoveRight(this._images[this._currentImage].style);
+            this._currentImage--;
+            this._imageMoveCenter(this._images[this._currentImage].style);
+            console.log("right", this._leftZIndex, this._rightZIndex);
+            this._leftZIndex--;
+            this._rightZIndex++;
+        }
     }
 
     private _resetArrayValues(imgs: HTMLCollectionOf<HTMLElement>) {
         for (let i = 0; i < imgs.length; i++) {
-            imgs[i].style.transform = "translateX(-200%)";
+            this._imageMoveRight(imgs[i].style);
         }
-        imgs[2].style.transform = "translateX(0)";
+        for (let i = 0; i < imgs.length; i++) {
+            imgs[i].style.zIndex = `${imgs.length - i}`;
+        }
+        this._imageMoveCenter(imgs[0].style);
     }
 }
