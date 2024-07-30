e55d230a536a308a8967e57e0e979241d97781c0
Arik Hativa <arikhativa@gmail.com>
2024-07-30 18:35:43 +0300

    read more animation works

--- src/app/read-more/read-more.component.ts
+++ src/app/read-more/read-more.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChildren } from '@angular/core';
 import { Link } from '../read-more-card/read-more-card.component';
 
 
@@ -16,5 +16,37 @@ export class ReadMoreComponent {
       id: "1",
     },
   ]
+  private observer!: IntersectionObserver;
+
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
+  ngAfterViewInit() {
+    const leftCards: NodeList = this.el.nativeElement.querySelectorAll('.left-card');
+    const rightCards: NodeList = this.el.nativeElement.querySelectorAll('.right-card');
+
+    this.observer = new IntersectionObserver((entries) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          this.renderer.addClass(entry.target, 'visible');
+        } else {
+          this.renderer.removeClass(entry.target, 'visible');
+        }
+      });
+    });
+
+    leftCards.forEach((card: any) => {
+      this.observer.observe(card);
+    });
+    rightCards.forEach((card: any) => {
+      this.observer.observe(card);
+    });
+  }
+
+  ngOnDestroy() {
+    if (this.observer) {
+      this.observer.disconnect();
+    }
+  }
+
 
 }
