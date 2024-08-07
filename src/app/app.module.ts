97f2183cedfb4f42d9ff16276906f5b342b51ad9
Arik Hativa <arikhativa@gmail.com>
2024-08-07 18:05:11 +0300

    added sitemap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BodyModule } from './body/body.module';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-// TODO AM auto added
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 
--- src/app/constants/strings.ts
+++ src/app/constants/strings.ts
@@ -1,16 +1,5 @@
-// TODO add all images
 export const imagesPaths = [
-    "/assets/images/gallery/1.jpg",
-    "/assets/images/gallery/2.jpg",
-    // "/assets/images/gallery/3.jpg",
-    // "/assets/images/gallery/4.jpg",
-    // "/assets/images/gallery/5.jpg",
-    // "/assets/images/gallery/6.jpg",
-    // "/assets/images/gallery/7.jpg",
-    // "/assets/images/gallery/8.jpg",
-    // "/assets/images/activities/1.jpg",
-    // "/assets/images/activities/2.jpg",
-    // "/assets/images/activities/3.jpg",
-    // "/assets/images/activities/4.jpg",
+    "/assets/images/about/toubi.jpg",
+    "/assets/images/about/house.jpg",
 ]
 
