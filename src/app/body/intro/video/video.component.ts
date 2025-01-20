6bf53e733ff5652b7fc9e44c879bed8cbdd4411a
Arik Hativa <arikhativa@gmail.com>
2025-01-20 17:21:12 +0200

    pause / paly on vid view

--- src/app/body/intro/video/video.component.ts
+++ src/app/body/intro/video/video.component.ts
@@ -1,18 +1,54 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  ViewChild,
+  AfterViewInit,
+  HostListener,
+} from '@angular/core';
 import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
-    selector: 'app-video',
-    templateUrl: './video.component.html',
-    styleUrl: './video.component.scss'
+  selector: 'app-video',
+  templateUrl: './video.component.html',
+  styleUrl: './video.component.scss',
 })
-export class VideoComponent {
-    icon = faCircleChevronDown
+export class VideoComponent implements AfterViewInit {
+  @ViewChild('videoIframe') videoIframe!: ElementRef;
+  icon = faCircleChevronDown;
 
-    scrollDown(): void {
-        const introElement = document.querySelector('app-about-toubi');
-        if (introElement) {
-            introElement.scrollIntoView({ behavior: 'smooth' });
-        }
+  ngAfterViewInit(): void {
+    this.checkVideoInView();
+  }
+
+  @HostListener('window:scroll', [])
+  onWindowScroll(): void {
+    this.checkVideoInView();
+  }
+
+  checkVideoInView(): void {
+    const rect = this.videoIframe.nativeElement.getBoundingClientRect();
+    const isInView =
+      rect.top >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight);
+    const iframe = this.videoIframe.nativeElement.contentWindow;
+    if (isInView) {
+      iframe.postMessage(
+        '{"event":"command","func":"playVideo","args":""}',
+        '*'
+      );
+    } else {
+      iframe.postMessage(
+        '{"event":"command","func":"pauseVideo","args":""}',
+        '*'
+      );
     }
+  }
+
+  scrollDown(): void {
+    const introElement = document.querySelector('app-about-toubi');
+    if (introElement) {
+      introElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
